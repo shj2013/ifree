c7cbccc19264688ffdebb812a6686c17ddd2ef33
suhuajun <418181705@qq.com>
2019-07-23 20:09:21 +0800

    t

---
+++ ifreeGroup/IfreeGroup.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ifree
+ * Date: 2019/7/23
+ * Time: 19:40
+ */
+
+namespace ifreeGroup;
+
+
+class IfreeGroup
+{
+    /**
+     * @author su@ifreegroup.com
+     * @date 2019/7/23  19:41
+     */
+    public static function json(){
+        echo 'we';
+    }
+}
